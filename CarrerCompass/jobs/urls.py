a80898742a137035c04f2b2f94c86e084c7c0533
paingzinmoe1214 <ko622941@gmail.com>
2024-08-29 00:08:43 +0630

    fixing bugs

--- CarrerCompass/jobs/urls.py
+++ CarrerCompass/jobs/urls.py
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 from django.urls import path 
 from . import views 
-=======
-from django.urls import path
-from . import views
->>>>>>> 11bd1ef5f14c64eda843754c944c09f05252ca3f
 
 urlpatterns = [
     path("", views.index, name="index")
