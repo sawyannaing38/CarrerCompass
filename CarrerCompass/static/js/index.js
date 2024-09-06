226002e3a9a36fe0f553ac441e0fb9a1b431a122
paingzinmoe1214 <ko622941@gmail.com>
2024-09-06 10:40:03 +0630

    fixing home page style

--- CarrerCompass/static/js/index.js
+++ CarrerCompass/static/js/index.js
@@ -2,6 +2,12 @@ import { toggleShow} from "./common.js";
 
 const heading = document.querySelector(".job-offer-heading")
 const jobs = document.querySelectorAll(".job");
+const jobLists = document.querySelector(".job-lists");
+
+if (jobs.length == 4)
+{
+    jobLists.style.justifyContent = "space-between";
+}
 
 document.addEventListener("scroll", function()
 {
